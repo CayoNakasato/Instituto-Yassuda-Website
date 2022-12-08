e2a1d5353de0207026dc1670c2919e6468e3b153
Igor Garcia <igorgg@gmail.com>
2022-12-08 18:28:29 -0300

    feat: carousel done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import { Flex } from "@chakra-ui/react";
+import { CarouselComponent } from "../components/Carousel";
 import { Header } from "../components/Header";
 
 export const HomePage = () => {
   return (
     <>
       <Header />
+      <Flex width={["100vw"]}>
+        <CarouselComponent />
+      </Flex>
     </>
   );
 };
