a2393441f5db63ed84c21c4379d1ec7337235539
Igor <igorgg@gmail.com>
2023-01-09 12:39:46 -0300

    hotfix: news positions

--- src/components/InfoHighlightsSection/index.tsx
+++ src/components/InfoHighlightsSection/index.tsx
@@ -1,24 +1,27 @@
-import { Box, Flex, Heading, Link } from "@chakra-ui/react";
+import { Divider, Flex, Heading, Link } from "@chakra-ui/react";
 import { InfoHighlightsBigCard } from "../InfoHighlightBigCard";
 import { InfoHighlightsSmallCard } from "../InfoHighlightSmallCard";
 
 export const InfoHighlightsSection = () => {
   return (
     <Flex flexDirection="column" gap="2rem" padding="2rem">
-      <Box id="highlights" position="relative" top="-100"></Box>
-
       <Heading width="180px" margin="0 auto">
         Notícias
       </Heading>
+      <Divider />
       <Flex
         flexDirection={["column", "column", "column", "column", "row", "row"]}
         justifyContent="space-evenly"
-        alignItems={["center", "center", "center", "center", "center"]}
+        alignItems={["center"]}
         padding="2rem"
       >
-        <Flex flexDirection={["row", "row", "row", "row", "column"]} gap="2rem">
+        <Flex
+          alignSelf={"flex-start"}
+          justifyContent="space-around"
+          flexDirection={["row", "row", "row", "row", "column"]}
+          gap="1rem"
+        >
           <InfoHighlightsBigCard path={"/"} />
-
           <Flex
             width={"100%"}
             display={["none", "none", "none", "none", "flex"]}
@@ -27,7 +30,7 @@ export const InfoHighlightsSection = () => {
           </Flex>
         </Flex>
 
-        <Flex gap="1rem" flexDirection={"column"} alignItems="center">
+        <Flex gap="1rem" flexDirection={"column"} alignSelf="flex-start">
           <Flex
             flexDirection={["row", "row", "row", "row", "column", "column"]}
             gap="2rem"
@@ -38,7 +41,7 @@ export const InfoHighlightsSection = () => {
             <InfoHighlightsSmallCard path={"/"} />
             <InfoHighlightsSmallCard path={"/"} />
           </Flex>
-          <Link href="/" color="blue" marginTop={"3rem"}>
+          <Link href="/" color="blue" marginTop={["0", "0", "3rem"]}>
             Veja mais notícias...
           </Link>
         </Flex>
