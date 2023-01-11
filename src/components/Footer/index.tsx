6f27397b0eb2cb34b84e851a1e1ebcac63fafee6
Cayo Nakasato <cayonakasato112@gmail.com>
2023-01-11 13:50:51 -0400

    fix: update files

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,7 @@
-<<<<<<< Updated upstream
 import { FaFacebook } from "react-icons/fa";
 import { AiFillInstagram, AiFillPhone } from "react-icons/ai";
 import { MdEmail } from "react-icons/md";
 import {
-  Box,
   Divider,
   Flex,
   Heading,
@@ -115,31 +113,5 @@ export const Footer = () => {
           <ContactForm />
       </Flex>
     </Flex>
-=======
-import { Link } from "@chakra-ui/react";
-import { ReactNode } from "react";
-
-interface ITopicsProps {
-  children: ReactNode;
-  to?: string;
-  path?: string
+  )
 }
-
-export const Topics = ({ children, to, path }: ITopicsProps) => {
-  return (
-    <Link
-      color="#504444"
-      paddingLeft="10px"
-      fontSize="15px"
-      textDecoration="none"
-      _hover={{
-        color: "red",
-        transition: "200ms",
-      }}
-      href={to}
-    >
-      {children}
-    </Link>
->>>>>>> Stashed changes
-  );
-};
