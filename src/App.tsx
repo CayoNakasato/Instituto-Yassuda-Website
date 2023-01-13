c33285200aa1cf3e20c667a9bb91227d3ccf7e4a
Igor <igorgg@gmail.com>
2023-01-13 10:29:43 -0300

    feat: no copy things

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import { Router } from "./routes/Routes";
 
 function App() {
-
+  const disableRightClick = (event: any) => {
+    if (event.type === "contextmenu") {
+      event.preventDefault();
+    } else if (
+      event.type === "keydown" &&
+      event.ctrlKey &&
+      event.keyCode === 67
+    ) {
+      event.preventDefault();
+    }
+  };
+  document.addEventListener("contextmenu", disableRightClick);
+  document.addEventListener("keydown", disableRightClick);
   return (
     <>
       <Router />
